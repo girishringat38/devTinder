097033b3cc4bdc56f58be0963791c4c1f10f8766
Girish Ringat <girishringat38@gmail.com>
2026-01-01 17:51:33 +0530

    wildCard-Error-handling-

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 app.use("/admin", adminAuth);
 
 app.get("/user/login", (req, res) => {
-  res.send("User login successfully");
+  try {
+    throw new Error("fgfsgfg");
+    res.send("User login successfully");
+  } catch (err) {
+    res.status(500).send("Something went wrong2");
+  }
 });
 
 app.get("/user", userAuth, (req, res) => {
@@ -21,6 +26,13 @@ app.get("/admin/getAllData", (req, res) => {
 
 app.get("/admin/deleteUser", (req, res) => {
   res.send("Delete a user");
+});
+
+//Error handling
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(3000, () => {
