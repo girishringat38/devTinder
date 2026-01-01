daa688e7223c050239c47e8cc549d9960d33ce85
Girish Ringat <girishringat38@gmail.com>
2026-01-01 20:20:20 +0530

    Init-database-

--- src/app.js
+++ src/app.js
@@ -1,40 +1,32 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
+app.post("/signup", async (req, res) => {
+  //Creating a new instance of the user model
+  const user = new User({
+    firstName: "Devansh",
+    lastName: "Ringat",
+    emailId: "devansh@gmail.com",
+    password: "devansh@123",
+  });
 
-//Handle Auth middleware for the all GET, POST,....requests
-app.use("/admin", adminAuth);
-
-app.get("/user/login", (req, res) => {
   try {
-    throw new Error("fgfsgfg");
-    res.send("User login successfully");
+    await user.save();
+    res.send("User added successfully");
   } catch (err) {
-    res.status(500).send("Something went wrong2");
+    res.status(400).send("Error saving the user: " + err.message);
   }
 });
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("User data sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Delete a user");
-});
-
-//Error handling
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something went wrong");
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000...");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected");
+  });
