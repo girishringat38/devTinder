215bdd623e4d9c0d0e024509f5854ad4b6ca5e22
Girish Ringat <girishringat38@gmail.com>
2026-01-01 17:06:53 +0530

    Auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,28 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the route user !!");
-    //res.send("Response !!");
+//Handle Auth middleware for the all GET, POST,....requests
+app.use("/admin", (req, res, next) => {
+  console.log("Admin auth is getting checked !!");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
     next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2 !!");
-    //res.send("2nd Response !!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 3 !!");
-    //res.send(" 3rd Response !!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4 !!");
-    res.send("4th Response !!");
   }
-);
+});
+
+app.get("/user", (req, res) => {
+  res.send("User data sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Delete a user");
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
