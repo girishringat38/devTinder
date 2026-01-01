b9a3f6ccafb2d822571631010840817ba6439f9a
Girish Ringat <girishringat38@gmail.com>
2026-01-01 14:57:55 +0530

    multiole-route-handler

--- src/app.js
+++ src/app.js
@@ -2,29 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Girish", lastName: "Ringat" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data Saved Successfully");
-});
-
-app.patch("/user", (req, res) => {
-  res.send(res.send("Data update Successfully"));
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Daya delete successfully");
-});
-
-app.put("/user", (req, res) => {
-  res.send("Data update save successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user !!");
+    //res.send("Response !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2 !!");
+    //res.send("2nd Response !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3 !!");
+    //res.send(" 3rd Response !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4 !!");
+    res.send("4th Response !!");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
