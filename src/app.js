467973493808a77756c341b4a8851bfaefe0690a
Girish Ringat <girishringat38@gmail.com>
2026-01-02 21:03:06 +0530

    patch-api-to-update-document

--- src/app.js
+++ src/app.js
@@ -21,12 +21,18 @@ app.post("/signup", async (req, res) => {
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
-    const user = await User.findOne({ emailId: userEmail });
+    const user = await User.findById({ _id: req.body._id });
     if (!user) {
       res.status(404).send("User not found");
     } else {
       res.send(user);
     }
+    // const user = await User.findOne({ emailId: userEmail });
+    // if (!user) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(user);
+    // }
     // const users = await User.find({ emailId: userEmail });
     // if (users.length === 0) {
     //   res.status(404).send("User not found");
@@ -47,6 +53,30 @@ app.get("/feed", async (req, res) => {
     } else {
       res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//delete a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//update the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data);
+    console.log(user);
+    res.send("User updated successfully");
   } catch (err) {
     res.status(400).send("Something went wrong");
   }
